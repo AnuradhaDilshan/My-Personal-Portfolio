937dd4c9629e0b5830b40f041e1de48b3bee4357
Anuradha <101551005+AnuradhaDilshan@users.noreply.github.com>
2025-04-17 12:43:31 +0530

    ReadmeViewer Build Error Fix

--- src/components/ReadmeViewer.tsx
+++ src/components/ReadmeViewer.tsx
@@ -5,6 +5,7 @@ import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import rehypeHighlight from "rehype-highlight";
+import type { Components } from "react-markdown";
 
 type Props = {
   url: string;
@@ -32,46 +33,48 @@ const ReadmeViewer: React.FC<Props> = ({ url }) => {
 
   const repoBaseUrl = url.split("/blob/")[0];
 
+  const customComponents: Components = {
+    a: ({ node, href, ...props }) => {
+      const adjustedHref =
+        href === "./LICENSE" ? `${repoBaseUrl}/blob/main/LICENSE` : href;
+
+      return (
+        <a
+          {...props}
+          href={adjustedHref}
+          className="text-blue-500 hover:underline"
+          target="_blank"
+          rel="noopener noreferrer"
+        />
+      );
+    },
+
+    code: ({ inline, className, children, ...props }) => {
+      const match = /language-(\w+)/.exec(className || "");
+      return !inline && match ? (
+        <code {...props} className={`${className} text-white-300`}>
+          {children}
+        </code>
+      ) : (
+        <code
+          {...props}
+          className={`bg-gray-800 text-white-300 rounded-md px-1 ${
+            className || ""
+          }`}
+        >
+          {children}
+        </code>
+      );
+    },
+  };
+
   return (
     <div className="markdown-body p-8">
       <ReactMarkdown
         children={markdown}
         remarkPlugins={[remarkGfm]}
         rehypePlugins={[rehypeRaw, rehypeHighlight]}
-        components={{
-          a: ({ node, href, ...props }) => {
-            const adjustedHref =
-              href === "./LICENSE" ? `${repoBaseUrl}/blob/main/LICENSE` : href;
-
-            return (
-              <a
-                {...props}
-                href={adjustedHref}
-                className="text-blue-500 hover:underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              />
-            );
-          },
-
-          code: ({ node, inline, className, children, ...props }) => {
-            const match = /language-(\w+)/.exec(className || "");
-            return !inline && match ? (
-              <code {...props} className={`${className} text-white-300`}>
-                {children}
-              </code>
-            ) : (
-              <code
-                {...props}
-                className={`bg-gray-800 text-white-300 rounded-md px-1 ${
-                  className || ""
-                }`}
-              >
-                {children}
-              </code>
-            );
-          },
-        }}
+        components={customComponents}
       />
     </div>
   );
