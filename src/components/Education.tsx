c5a9baed6290a4e5e35d968ef3dab01608c734b2
Anuradha <101551005+AnuradhaDilshan@users.noreply.github.com>
2025-04-25 00:07:31 +0530

    fixed application error: a client-side #4

--- src/components/Education.tsx
+++ src/components/Education.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import React from "react";
 import { EduGrid } from "./ui/EduGrid";
 import { TracingBeam } from "./ui/TracingBeam";
--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,3 +1,5 @@
+import React from "react";
+
 import { softwares, workExperience } from "../../data/index";
 import { Button } from "./ui/MovingBorders";
 
