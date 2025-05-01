417fe554170e0c83f5b91b573327f1f18b170d60
Anuradha <101551005+AnuradhaDilshan@users.noreply.github.com>
2025-05-01 17:50:25 +0530

    minor changes #2

--- src/app/projects/[slug]/page.tsx
+++ src/app/projects/[slug]/page.tsx
@@ -5,6 +5,19 @@ import { project_data } from "../../../../data/index";
 import NotFound from "@/app/not-found";
 import Projects from "@/components/Projects";
 
+// Keep the Project interface for documentation
+interface Project {
+  id: number;
+  title: string;
+  des: string;
+  img: string;
+  iconLists: { icon: string; name: string }[];
+  visit?: { icon: string; link: string }[];
+  image?: string;
+  image_ui?: string;
+  readme?: string;
+}
+
 const ProjectsPage = () => {
   const { slug } = useParams();
 
@@ -22,7 +35,7 @@ const ProjectsPage = () => {
 
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col mx-auto sm:px-10 px-5 overflow-clip">
-      <Projects />
+      <Projects project={project} />
     </main>
   );
 };
--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -50,7 +50,7 @@ const Blog: React.FC = () => {
         My <span className="text-purple-500">Blog</span>
       </h1>
 
-      <div className="flex flex-col items-center -mt-10 -mb-30">
+      <div className="flex flex-col items-center">
         <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center justify-center relative overflow-hidden">
           <InfiniteMovingCards
             items={transformedBlogs}
