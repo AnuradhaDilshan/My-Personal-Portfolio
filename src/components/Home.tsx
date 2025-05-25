aa71b2774504b86adf20856337c73a5b77a59560
Anuradha <101551005+AnuradhaDilshan@users.noreply.github.com>
2025-05-26 04:57:32 +0530

    minor changes #5

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -56,7 +56,7 @@ const Home = () => {
           </p>
 
           <a
-            href="https://drive.google.com/file/d/1qwg6aAE1lgVxmpBklNHLWAxNkKgHbtKg/view?usp=sharing"
+            href="https://drive.google.com/file/d/1ycZEoOpmryWOEWclvlhCL1xSnWz50PZx/view?usp=sharing"
             target="_blank"
             rel="noopener noreferrer"
             className="mt-1 fade-in animation-delay-600"
