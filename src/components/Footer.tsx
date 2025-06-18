bc7eeb524b4e4e3d786cdf4191ed3cfd3cc04faf
Anuradha <101551005+AnuradhaDilshan@users.noreply.github.com>
2025-06-18 20:48:16 +0530

    minor changes #7

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -56,7 +56,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:anuradha256kcc@gmail.com">
+        <a href="mailto:anuradhadilshan.dev@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
